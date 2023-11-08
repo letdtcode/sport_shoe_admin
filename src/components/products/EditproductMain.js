88a90613153fe25d10b98d5e22076ff2d5931225
username <tuandangvann@gmail.com>
2023-11-08 14:18:16 +0700

    edit product

--- src/components/products/EditproductMain.js
+++ src/components/products/EditproductMain.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Toast from "../LoadingError/Toast";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,8 +10,20 @@ import {
 import Loading from "../LoadingError/Loading";
 import Message from "../LoadingError/Error";
 import { PRODUCT_UPDATE_RESET } from "../../redux/constants/ProductConstants";
-import { Heading, Select } from "@chakra-ui/react";
 import { categoryListAllAction } from "../../redux/actions/CategoryAction";
+import styled from "@emotion/styled";
+import {
+  Button,
+  Heading,
+  Select,
+  Table,
+  Input,
+  Th,
+  Td,
+  Thead,
+  Tr,
+  Tbody,
+} from "@chakra-ui/react";
 const ToastObjects = {
   pauseOnFocusLoss: false,
   draggable: false,
@@ -19,15 +31,69 @@ const ToastObjects = {
   autoClose: 2000,
 };
 
+const BtnPrimary = styled.button`
+  padding: 8px 45px;
+  background-color: #333;
+  font-size: 18px;
+  color: white;
+`;
+
 const EditProductMain = (props) => {
   const { productId } = props;
 
   // Set up state
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
-  const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
+  const inputRef = useRef(null);
+  const [imageFile, setImageFile] = useState(null);
+  const handleImgChange = (e) => {
+    setImageFile(e.target.files[0])
+    // inputRef.current.value = '';
+};
+const [itemTypes, setItemTypes] = useState([""]);
+
+  const handleItemType = (index, value) => {
+    const newItemType = [...itemTypes];
+    newItemType[index] = value;
+    newItemType[index + 1] = newItemType[index + 1]
+      ? newItemType[index + 1]
+      : "";
+    if (value === "" && index < newItemType.length - 1) {
+      newItemType.splice(index, 1);
+    }
+    setItemTypes(newItemType);
+  };
+  const [selectedSizes, setSelectedSizes] = useState([]);
+  const availableSizes = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45];
+
+  const handleSizeChange = (size) => {
+    if (selectedSizes.includes(size)) {
+      // Nếu size đã được chọn, loại bỏ nó khỏi danh sách selectedSizes
+      setSelectedSizes(selectedSizes.filter((item) => item !== size));
+    } else {
+      // Nếu size chưa được chọn, thêm nó vào danh sách selectedSizes
+      setSelectedSizes([...selectedSizes, size]);
+    }
+  };
+  const isSizeSelected = (size) => selectedSizes.includes(size);
+
+  const [quantityData, setQuantityData] = useState([[]]);
+  const addQuantity = (itemTypes, selectedSizes) => {
+    const updatedQuantityData = [];
+    for (let i = 0; i < itemTypes.length - 1; i++) {
+      const row = [];
+      for (let j = 0; j < selectedSizes.length; j++) {
+        row.push(0);
+      }
+      updatedQuantityData.push(row);
+    }
+    setQuantityData(updatedQuantityData);
+  };
+  useEffect(() => {
+    addQuantity(itemTypes, selectedSizes);
+    console.log(quantityData);
+  }, [itemTypes, selectedSizes]);
 
   // Declare Dispatch
   const dispatch = useDispatch();
@@ -55,9 +121,8 @@ const EditProductMain = (props) => {
       } else {
         setName(product.name);
         setPrice(product.price);
-        setCountInStock(product.countInStock);
         setDescription(product.description);
-        setImage(product.image);
+        setImageFile(product.image);
         setCategory(product.category);
       }
     }
@@ -71,8 +136,7 @@ const EditProductMain = (props) => {
         name,
         price,
         description,
-        countInStock,
-        image,
+        imageFile,
         category,
       })
     );
@@ -99,101 +163,174 @@ const EditProductMain = (props) => {
             <div className="col-xl-8 col-lg-8">
               <div className="card mb-4 shadow-sm">
                 <div className="card-body">
-                  {errorUpdate && (
-                    <Message variant="alert-danger">{errorUpdate}</Message>
-                  )}
-                  {loadingUpdate && <Loading />}
-                  {loading ? (
-                    <Loading />
-                  ) : error ? (
-                    <Message variant="alert-danger">{error}</Message>
-                  ) : (
-                    <>
-                      <div className="mb-4">
-                        <label htmlFor="product_title" className="form-label">
-                          Product's name
-                        </label>
-                        <input
-                          type="text"
-                          placeholder="Type here"
-                          className="form-control"
-                          id="product_title"
-                          required
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                        />
+                  {error && <Message variant="alert-danger">{error}</Message>}
+                  {loading && <Loading />}
+                  <div className="mb-4">
+                    <label htmlFor="product_title" className="form-label">
+                      Product's name
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter the product name"
+                      className="form-control"
+                      id="product_title"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="product_price" className="form-label">
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      placeholder="Type here"
+                      className="form-control"
+                      id="product_price"
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                      required
+                    />
+                  </div>
+                  {/* <div className="mb-4">
+                    <label htmlFor="product_quantity" className="form-label">
+                      Quantity
+                    </label>
+                    <input
+                      type="number"
+                      placeholder="Type here"
+                      className="form-control"
+                      id="product_quantity"
+                      value={countInStock}
+                      onChange={(e) => setCountInStock(e.target.value)}
+                      required
+                    />
+                  </div> */}
+                  <div className="mb-4">
+                    <label htmlFor="product_category" className="form-label">
+                      Category
+                    </label>
+                    <Select
+                      placeholder="Choose a category"
+                      value={category}
+                      onChange={(e) => console.log(setCategory(e.target.value))}
+                    >
+                      {categories?.map((item) => (
+                        <option key={item._id} value={item._id}>
+                          {item.name}
+                        </option>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Description</label>
+                    <textarea
+                      placeholder="Enter product description"
+                      className="form-control"
+                      rows="7"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Image</label>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      ref={inputRef}
+                      onChange={handleImgChange}
+                    />
+                  </div>
+
+                  <div></div>
+                  <div className="mb-4">
+                    <div style={{ display: "grid" }}>
+                      <label>Phân loại màu</label>
+                      <div>
+                        {itemTypes.map((itemTpye, index) => (
+                          <input
+                            width={"100px"}
+                            key={index}
+                            type="text"
+                            value={itemTpye}
+                            placeholder="Type here"
+                            className="input-item-type"
+                            id="type"
+                            onChange={(e) =>
+                              handleItemType(index, e.target.value)
+                            }
+                          />
+                        ))}
                       </div>
-                      <div className="mb-4">
-                        <label htmlFor="product_price" className="form-label">
-                          Price
-                        </label>
-                        <input
-                          type="number"
-                          placeholder="Type here"
-                          className="form-control"
-                          id="product_price"
-                          required
-                          value={price}
-                          onChange={(e) => setPrice(e.target.value)}
-                        />
+                    </div>
+                    <div style={{ display: "grid" }}>
+                      <label>Phân loại size</label>
+                      <div>
+                        {availableSizes.map((size) => (
+                          <button
+                            key={size}
+                            className={
+                              isSizeSelected(size)
+                                ? "button-size-selected"
+                                : "button-size"
+                            }
+                            required={false}
+                            onClick={() => handleSizeChange(size)}
+                          >
+                            {size}
+                          </button>
+                        ))}
                       </div>
-                      <div className="mb-4">
-                        <label htmlFor="product_price" className="form-label">
-                          Quantity
-                        </label>
-                        <input
-                          type="number"
-                          placeholder="Type here"
-                          className="form-control"
-                          id="product_price"
-                          required
-                          value={countInStock}
-                          onChange={(e) => setCountInStock(e.target.value)}
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          htmlFor="product_category"
-                          className="form-label"
-                        >
-                          Category
-                        </label>
-                        <Select
-                          placeholder="Select a category"
-                          value={category}
-                          onChange={(e) => setCategory(e.target.value)}
-                        >
-                          {categories?.map((item) => (
-                            <option key={item._id} value={item.name}>
-                              {item.name}
-                            </option>
-                          ))}
-                        </Select>
-                      </div>
-                      <div className="mb-4">
-                        <label className="form-label">Description</label>
-                        <textarea
-                          placeholder="Type here"
-                          className="form-control"
-                          rows="7"
-                          required
-                          value={description}
-                          onChange={(e) => setDescription(e.target.value)}
-                        ></textarea>
-                      </div>
-                      <div className="mb-4">
-                        <label className="form-label">Image</label>
-                        <input
-                          className="form-control"
-                          type="text"
-                          value={image}
-                          onChange={(e) => setImage(e.target.value)}
-                        />
-                      </div>
-                    </>
-                  )}
+                    </div>
+
+                    <Table variant="simple">
+                      <Thead>
+                        <Tr>
+                          <Th>Type</Th>
+                          <Th>Size</Th>
+                          <Th>Quantity</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {quantityData.map((type, i) => (
+                          <>
+                            {type.map((size, j) => (
+                              <>
+                                <Tr>
+                                  <Td>{itemTypes[i]}</Td>
+                                  <Td>{selectedSizes[j]}</Td>
+                                  <Td>
+                                    <Input
+                                      type="number"
+                                      id={`quantity${i}${j}`}
+                                      value={quantityData[i][j]}
+                                      onChange={(e) => {
+                                        const updatedQuantityData = [
+                                          ...quantityData,
+                                        ];
+                                        updatedQuantityData[i][j] = parseInt(
+                                          e.target.value,
+                                          10
+                                        );
+                                        setQuantityData(updatedQuantityData);
+                                      }}
+                                    />
+                                  </Td>
+                                </Tr>
+                              </>
+                            ))}
+                          </>
+                        ))}
+                      </Tbody>
+                    </Table>
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="d-flex justify-content-end">
+              <BtnPrimary type="submit">Publish now</BtnPrimary>
             </div>
           </div>
         </form>
